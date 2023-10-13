04a36e01070f3470bebd3cb1c18684ec9170a4e5
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2023-10-13 13:13:31 -0300

    update message

--- src/pages/services-offline.tsx
+++ src/pages/services-offline.tsx
@@ -1,22 +1,26 @@
 import { useServiceStatus } from '@/contexts/ServiceStatusContext'
-import { Center, Heading, Text } from '@chakra-ui/react'
+import { Box, Center, Heading, Text, VStack } from '@chakra-ui/react'
 
 export function ServicesOffline() {
   const { serverOnline } = useServiceStatus()
 
   return serverOnline === false ? (
     <Center w="100%" h="100%">
-      Servidor indisponível.
+      Servidor de jogo indisponível.
     </Center>
   ) : (
     <Center flexDir="column" w="100%" h="100%">
-      <Heading>Aguarde enquanto religamos o servidor...</Heading>
-      <Text>
-        O servidor do Magic3t é desligado automaticamente quando inativo para
-        reduzir gastos, mas já iniciamos o processo de religamento. Esse
-        processo leva cerca de 2 a 3 minutos e te avisaremos quando tudo estiver
-        pronto.
-      </Text>
+      <VStack maxW="600px" gap="20px">
+        <Heading textAlign="center">
+          Aguarde enquanto religamos o servidor...
+        </Heading>
+        <Center textAlign="center">
+          O servidor do Magic3t é desligado automaticamente quando inativo para
+          reduzir gastos, mas já iniciamos o processo de religamento. Esse
+          processo leva cerca de 2 a 3 minutos e te avisaremos quando tudo
+          estiver pronto.
+        </Center>
+      </VStack>
     </Center>
   )
 }
