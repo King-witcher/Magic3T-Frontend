66630e6689c2b01d21ba1862a270911faf2f8d9e
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2023-10-19 19:33:20 -0300

    add server status refresh

--- src/contexts/ServiceStatusContext.tsx
+++ src/contexts/ServiceStatusContext.tsx
@@ -4,6 +4,7 @@ import {
   createContext,
   useContext,
   useEffect,
+  useRef,
   useState,
 } from 'react'
 
@@ -21,18 +22,26 @@ const ServiceStatusContext = createContext<ServiceStatusData>({
 
 export function ServiceStatusProvider({ children }: Props) {
   const [serverOnline, setServerOnline] = useState<boolean>()
+  const timeoutRef = useRef<NodeJS.Timeout>()
 
   async function fetchStatus() {
     try {
       const response = await axios.get(`${import.meta.env.VITE_API_URL}/status`)
-      if (response.data.status === 'available') setServerOnline(true)
+      if (response.data.status === 'available') {
+        setServerOnline(true)
+        timeoutRef.current = setTimeout(fetchStatus, 5000)
+      }
     } catch {
       setServerOnline(false)
+      timeoutRef.current = setTimeout(fetchStatus, 2000)
     }
   }
 
   useEffect(() => {
     fetchStatus()
+    return () => {
+      clearTimeout(timeoutRef.current)
+    }
   }, [])
 
   return (
