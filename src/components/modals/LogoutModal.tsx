c9b329896c9f09db011fe27ff45202b061e3f7c1
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2023-10-19 12:48:23 -0300

    logout modal

---
+++ src/components/modals/LogoutModal.tsx
@@ -0,0 +1,39 @@
+import { useAuth } from '@/contexts/AuthContext'
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ModalProps,
+} from '@chakra-ui/react'
+
+export default function LogoutModal(props: Omit<ModalProps, 'children'>) {
+  const { signOut } = useAuth()
+
+  return (
+    <Modal isCentered {...props}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Sair agora?</ModalHeader>
+        <ModalBody>Deseja mesmo encerrar a sua sessão em Magic3T?</ModalBody>
+        <ModalFooter>
+          <Button onClick={props.onClose}>Cancelar</Button>
+          <Button
+            colorScheme="red"
+            onClick={async () => {
+              signOut()
+              props.onClose()
+            }}
+          >
+            Sair
+          </Button>
+        </ModalFooter>
+        <ModalCloseButton />
+      </ModalContent>
+    </Modal>
+  )
+}
