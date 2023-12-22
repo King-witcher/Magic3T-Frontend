90b62397f68693d17c061ece1adb73558025bf7f
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2023-12-21 22:31:12 -0300

    update ui

--- src/pages/game/[gameId]/components/PlayerCard.tsx
+++ src/pages/game/[gameId]/components/PlayerCard.tsx
@@ -86,16 +86,20 @@ export default function PlayerCard({ player, chatInputRef }: Props) {
         >
           <Avatar src={profile?.photoURL} size="lg" />
           <Stack gap="0">
-            <Text fontSize="20px" fontWeight={700} noOfLines={1}>
-              {profile?.nickname}
-            </Text>
-            <Flex alignItems="center" gap="5px">
-              <Image src={rating?.thumbnail} w="25px" />
-              <Text fontSize="16px">
-                {rating?.rating}
-                {(rating?.deviation || 200) > 100 && '*'} SR
-              </Text>
-            </Flex>
+            {profile && (
+              <>
+                <Text fontSize="20px" fontWeight={700} noOfLines={1}>
+                  {profile.nickname}
+                </Text>
+                <Flex alignItems="center" gap="5px">
+                  <Image src={rating?.thumbnail} w="25px" />
+                  <Text fontSize="16px">
+                    {rating!.rating}
+                    {(rating!.deviation || 200) > 100 && '*'} SR
+                  </Text>
+                </Flex>
+              </>
+            )}
           </Stack>
         </Center>
       </MenuButton>
