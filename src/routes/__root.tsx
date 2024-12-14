56812e4115e8d76d2ef3cf2ef8a8fff0b5d66a9e
Giuseppe Lanna <giuseppe@orium.network>
2024-12-14 13:52:49 -0300

    hotfix/remove tanstack router devtools from production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,20 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { Providers } from './-providers'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ErrorTemplate, RootLayout } from '@/components/templates'
+import React from 'react'
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      )
 
 export const Route = createRootRoute({
   component: () => (
