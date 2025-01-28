823b4186f5af1b92be7ef8d5a100fb0d89ab015a
Giuseppe Lanna <giuseppe@orium.network>
2025-01-27 22:12:45 -0300

    add loading component

--- src/routes/users/$nickname.tsx
+++ src/routes/users/$nickname.tsx
@@ -1,10 +1,17 @@
-import { NotFoundTemplate, ProfileTemplate } from '@/components/templates'
+import {
+  Loading,
+  NotFoundTemplate,
+  ProfileTemplate,
+} from '@/components/templates'
 import { NestApi } from '@/services/nest-api'
 import { useQuery, useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/users/$nickname')({
   component: RouteComponent,
+  pendingComponent: () => <Loading />,
+  notFoundComponent: () => <NotFoundTemplate />,
+  shouldReload: false,
 })
 
 function RouteComponent() {
