cbf906c1f054330adf28aa73216fc4cb489e2aa7
Giuseppe Lanna <giuseppe@orium.network>
2025-02-16 14:22:53 -0300

    remove chakra from root layout

--- src/components/templates/root-layout/root-layout.tsx
+++ src/components/templates/root-layout/root-layout.tsx
@@ -1,8 +1,8 @@
 import { PageWidthLimiter } from '@/components/atoms'
 import { Navbar } from '@/components/organisms'
-import { Box } from '@chakra-ui/react'
 import type { ReactNode } from 'react'
 import { ModalLayout } from './modal-layout'
+import styles from './styles.module.sass'
 
 interface Props {
   children: ReactNode
@@ -11,19 +11,7 @@ interface Props {
 export function RootLayout({ children }: Props) {
   return (
     <>
-      <Box
-        w="100vw"
-        h="100vh"
-        rounded={999999}
-        bg="radial-gradient(white, transparent 70.7%)"
-        mixBlendMode="overlay"
-        aspectRatio={1}
-        pos="fixed"
-        pointerEvents="none"
-        left="50%"
-        top="50%"
-        transform="translate(-50%, -50%)"
-      />
+      <div className={styles.brightness_container} />
       <div className="root-layout flex flex-col items-center h-dvh relative">
         <Navbar />
         <div className="flex flex-[1] w-full justify-center h-fit overflow-x-hidden overflow-y-scroll">
