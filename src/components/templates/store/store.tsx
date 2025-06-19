7371d1d6607062d5da96bd01da1b4eb75d835833
Giuseppe Lanna <giuseppe@orium.network>
2025-06-18 22:29:37 -0300

    fix: fix review comments

--- src/components/templates/store/store.tsx
+++ src/components/templates/store/store.tsx
@@ -14,7 +14,8 @@ export function StoreTemplate() {
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {icons.map((icon) => (
           <div key={icon} className="acrylic overflow-hidden shadow-md">
-            <img alt="img" src={getIconUrl(icon)} />
+            <img alt={`Icon ${icon}`} src={getIconUrl(icon)} />
+
             <h2 className="text-xl font-semibold">Item {icon}</h2>
             <p className="text-sm text-gray-600">Description of item {icon}</p>
             <button
